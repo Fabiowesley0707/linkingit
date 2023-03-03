6d1bac6e397893375c86e6bea8d4ad4de853268a
fabiowesley0707 <fabiowesley0702@gmail.com>
2023-03-02 21:43:26 -0400

    enviando meu primeiro pacote git

---
+++ src/linkingit/TestGit.java
@@ -0,0 +1,10 @@
+package linkingit;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Meu primeiro teste git");
+	}
+
+}
